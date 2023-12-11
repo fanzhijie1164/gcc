933c4468ce969dc7844cb23bcca2fba31d08772f
Jason Merrill <jason@redhat.com>
2023-12-11 11:35:31 -0500

    tree: add to clobber_kind
    In discussion of PR71093 it came up that more clobber_kind options would be
    useful within the C++ front-end.
    
    gcc/ChangeLog:
    
    	* tree-core.h (enum clobber_kind): Rename CLOBBER_EOL to
    	CLOBBER_STORAGE_END.  Add CLOBBER_STORAGE_BEGIN,
    	CLOBBER_OBJECT_BEGIN, CLOBBER_OBJECT_END.
    	* gimple-lower-bitint.cc
    	* gimple-ssa-warn-access.cc
    	* gimplify.cc
    	* tree-inline.cc
    	* tree-ssa-ccp.cc: Adjust for rename.
    	* tree-pretty-print.cc: And handle new values.
    
    gcc/cp/ChangeLog:
    
    	* call.cc (build_trivial_dtor_call): Use CLOBBER_OBJECT_END.
    	* decl.cc (build_clobber_this): Take clobber_kind argument.
    	(start_preparsed_function): Pass CLOBBER_OBJECT_BEGIN.
    	(begin_destructor_body): Pass CLOBBER_OBJECT_END.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/pr87052.c: Adjust expected CLOBBER output.
    
    Co-authored-by: Nathaniel Shead  <nathanieloshead@gmail.com>

--- gcc/cp/decl.cc
+++ gcc/cp/decl.cc
@@ -16984,7 +16984,7 @@ implicit_default_ctor_p (tree fn)
    storage is dead when we enter the constructor or leave the destructor.  */
 
 static tree
-build_clobber_this ()
+build_clobber_this (clobber_kind kind)
 {
   /* Clobbering an empty base is pointless, and harmful if its one byte
      TYPE_SIZE overlays real data.  */
@@ -17000,7 +17000,7 @@ build_clobber_this ()
   if (!vbases)
     ctype = CLASSTYPE_AS_BASE (ctype);
 
-  tree clobber = build_clobber (ctype);
+  tree clobber = build_clobber (ctype, kind);
 
   tree thisref = current_class_ref;
   if (ctype != current_class_type)
@@ -17419,7 +17419,7 @@ start_preparsed_function (tree decl1, tree attrs, int flags)
 	 because part of the initialization might happen before we enter the
 	 constructor, via AGGR_INIT_ZERO_FIRST (c++/68006).  */
       && !implicit_default_ctor_p (decl1))
-    finish_expr_stmt (build_clobber_this ());
+    finish_expr_stmt (build_clobber_this (CLOBBER_OBJECT_BEGIN));
 
   if (!processing_template_decl
       && DECL_CONSTRUCTOR_P (decl1)
@@ -17647,7 +17647,8 @@ begin_destructor_body (void)
 	    finish_decl_cleanup (NULL_TREE, stmt);
 	  }
 	else
-	  finish_decl_cleanup (NULL_TREE, build_clobber_this ());
+	  finish_decl_cleanup (NULL_TREE,
+			       build_clobber_this (CLOBBER_OBJECT_END));
       }
 
       /* And insert cleanups for our bases and members so that they
