db4d357db448c9e3b6e5e5e9a8da44d86b441092
Richard Biener <rguenther@suse.de>
2024-10-07 11:05:17 +0200

    tree-optimization/116982 - analyze scalar loop exit early
    The following makes sure to discover the scalar loop IV exit during
    analysis as failure to do so (if DCE and friends are disabled this
    can happen due to if-conversion doing DCE and FRE on the if-converted
    loop) would ICE later.
    
    I refrained from larger refactoring to be able to eventually backport.
    
    	PR tree-optimization/116982
    	* tree-vectorizer.h (vect_analyze_loop): Pass in .LOOP_VECTORIZED
    	call.
    	(vect_analyze_loop_form): Likewise.
    	* tree-vect-loop.cc (vect_analyze_loop_form): Reject loops where we
    	cannot determine a IV exit for the scalar loop.
    	(vect_analyze_loop): Adjust.
    	* tree-vectorizer.cc (try_vectorize_loop_1): Likewise.
    	* tree-parloops.cc (gather_scalar_reductions): Likewise.

--- gcc/tree-vect-loop.cc
+++ gcc/tree-vect-loop.cc
@@ -1342,7 +1342,8 @@ vect_compute_single_scalar_iteration_cost (loop_vec_info loop_vinfo)
      niter could be analyzed under some assumptions.  */
 
 opt_result
-vect_analyze_loop_form (class loop *loop, vect_loop_form_info *info)
+vect_analyze_loop_form (class loop *loop, gimple *loop_vectorized_call,
+			vect_loop_form_info *info)
 {
   DUMP_VECT_SCOPE ("vect_analyze_loop_form");
 
@@ -1352,6 +1353,18 @@ vect_analyze_loop_form (class loop *loop, vect_loop_form_info *info)
 				   "not vectorized:"
 				   " could not determine main exit from"
 				   " loop with multiple exits.\n");
+  if (loop_vectorized_call)
+    {
+      tree arg = gimple_call_arg (loop_vectorized_call, 1);
+      class loop *scalar_loop = get_loop (cfun, tree_to_shwi (arg));
+      edge scalar_exit_e = vec_init_loop_exit_info (scalar_loop);
+      if (!scalar_exit_e)
+	return opt_result::failure_at (vect_location,
+				       "not vectorized:"
+				       " could not determine main exit from"
+				       " loop with multiple exits.\n");
+    }
+
   info->loop_exit = exit_e;
   if (dump_enabled_p ())
       dump_printf_loc (MSG_NOTE, vect_location,
@@ -1429,7 +1442,7 @@ vect_analyze_loop_form (class loop *loop, vect_loop_form_info *info)
 
       /* Analyze the inner-loop.  */
       vect_loop_form_info inner;
-      opt_result res = vect_analyze_loop_form (loop->inner, &inner);
+      opt_result res = vect_analyze_loop_form (loop->inner, NULL, &inner);
       if (!res)
 	{
 	  if (dump_enabled_p ())
@@ -2981,7 +2994,8 @@ vect_analyze_loop_1 (class loop *loop, vec_info_shared *shared,
    for it.  The different analyses will record information in the
    loop_vec_info struct.  */
 opt_loop_vec_info
-vect_analyze_loop (class loop *loop, vec_info_shared *shared)
+vect_analyze_loop (class loop *loop, gimple *loop_vectorized_call,
+		   vec_info_shared *shared)
 {
   DUMP_VECT_SCOPE ("analyze_loop_nest");
 
@@ -2999,7 +3013,8 @@ vect_analyze_loop (class loop *loop, vec_info_shared *shared)
 
   /* Analyze the loop form.  */
   vect_loop_form_info loop_form_info;
-  opt_result res = vect_analyze_loop_form (loop, &loop_form_info);
+  opt_result res = vect_analyze_loop_form (loop, loop_vectorized_call,
+					   &loop_form_info);
   if (!res)
     {
       if (dump_enabled_p ())
