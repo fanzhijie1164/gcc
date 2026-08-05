b3ae9ec5ab224b7ca973dc656cf94787fa12f847
fanzhijie <fanzhijie1164@phytium.com.cn>
2026-08-05 21:00:56 +0800

    Sync vector tests with GCC 15

--- gcc/testsuite/gcc.dg/vect/vect-106.c
+++ gcc/testsuite/gcc.dg/vect/vect-106.c
@@ -1,4 +1,5 @@
 /* { dg-require-effective-target vect_int } */
+/* { dg-additional-options "-fdump-tree-optimized-details-blocks" } */
 
 #include <stdlib.h>
 #include <stdarg.h>
@@ -71,3 +72,5 @@ int main (void)
 }
 
 /* { dg-final { scan-tree-dump-times "vectorized 2 loops" 1 "vect" } } */
+
+/* { dg-final { scan-tree-dump-not "Invalid sum" "optimized" } } */
--- gcc/testsuite/gcc.dg/vect/vect-66.c
+++ gcc/testsuite/gcc.dg/vect/vect-66.c
@@ -1,4 +1,5 @@
 /* { dg-require-effective-target vect_int } */
+/* { dg-additional-options "-fdump-tree-optimized-details-blocks" } */
 
 #include <stdarg.h>
 #include "tree-vect.h"
@@ -96,3 +97,4 @@ int main (void)
 }
 
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 3 "vect" } } */
+/* { dg-final { scan-tree-dump-not "Invalid sum" "optimized" } } */
